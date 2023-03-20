170046214c058d49821e2f67237e8e45a0b11e7e
krr-hws <kramer@hw-schule.de>
2023-03-20 19:19:12 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "mbed.h"
+
+PortOut motor(PortC, 0b1111);
+unsigned char schritt[] = {0b0011, 0b0110, 0b1100, 0b1001};
+
+int main()
+{
+    while (true) {
+        for(int i = 0; i < 4; i++){
+            motor = schritt[i];
+            ThisThread::sleep_for(500ms);
+        }
+    }
+}
