ad54b2fb240c2df48697cf68f2c6e3706fac4e2c
krr-hws <kramer@hw-schule.de>
2023-03-21 07:27:06 +0000

    testkommentar

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ unsigned char schritt[] = {0b0011, 0b0110, 0b1100, 0b1001};
 int main()
 {
     while (true) {
+        //testkommentar
         for(int i = 0; i < 4; i++){
             motor = schritt[i];
             ThisThread::sleep_for(500ms);
